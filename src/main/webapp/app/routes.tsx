9eeb1b959ab70e3ba3bb268e83fdda536297bb45
Matt Raible <matt.raible@okta.com>
2018-06-26 08:35:12 -0600

    Deploy to Heroku

--- src/main/webapp/app/routes.tsx
+++ src/main/webapp/app/routes.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import { Switch } from 'react-router-dom';
+import * as Loadable from 'react-loadable';
 
 import Logout from 'app/modules/login/logout';
 import Home from 'app/modules/home/home';
-import Admin from 'app/modules/administration';
 import Entities from 'app/entities';
 import PrivateRoute from 'app/shared/auth/private-route';
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 import { AUTHORITIES } from 'app/config/constants';
+
+const Admin = Loadable({
+  loader: () => import('app/modules/administration'),
+  loading: () => <div>loading ...</div>
+});
 
 const Routes = () => (
   <div className="view-routes">
